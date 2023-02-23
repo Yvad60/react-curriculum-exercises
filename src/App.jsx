64fcc0f7964a2b3d4747f213b4b1ba45acc7729a
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-02-23 19:14:28 +0200

    Setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <h1 className="text-2xl text-red-500">Hello React!</h1>
     </div>
   );
 }
-
-export default App;
