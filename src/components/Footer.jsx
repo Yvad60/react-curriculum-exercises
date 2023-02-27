78bb3411ea6b6e534b4f95f1fa690c84075c67f8
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-02-27 13:18:33 +0200

    complete digital business card

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#161619] rounded-[10px]">
+      <div className="flex justify-center gap-6 py-5">
+        <i class="bi bi-twitter w-6 h-6 flex justify-center items-center bg-[#918E9B] rounded-sm"></i>
+        <i class="bi bi-facebook w-6 h-6 flex justify-center items-center bg-[#918E9B] rounded-sm"></i>
+        <i class="bi bi-instagram w-6 h-6 flex justify-center items-center bg-[#918E9B] rounded-sm"></i>
+        <i class="bi bi-github w-6 h-6 flex justify-center items-center bg-[#918E9B] rounded-sm"></i>
+      </div>
+    </footer>
+  );
+}
