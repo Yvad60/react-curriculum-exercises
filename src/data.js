0b9c11ced8a3f512b0e142d06a28b103473dfc4a
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-01 16:18:26 +0200

    update places with relevant information

--- src/data.js
+++ src/data.js
@@ -12,22 +12,22 @@ const places = [
   {
     name: "Nyandungu park",
     country: "Rwanda",
-    googleMapLink: "https://goo.gl/maps/CenfZ3CCFw7HDc7A9",
+    googleMapLink: "https://goo.gl/maps/TRW3oeMPswBQmYw69",
     startDate: "20 April, 2023",
-    endDate: "30 May, 2023",
-    image: "fazenda.jpg",
+    endDate: "21 May, 2023",
+    image: "nyandungu.jpg",
     description:
       "Nyandungu Urban Wetland Ecotourism park is a Rwandan tourism park located between Gasabo and Kicukiro Districts which allows sustainable travel of people to enjoy natural areas and wild animals in Nyandungu Valley.",
   },
   {
-    name: "Fazenda",
+    name: "Volcanoes National Park",
     country: "Rwanda",
-    googleMapLink: "https://goo.gl/maps/CenfZ3CCFw7HDc7A9",
-    startDate: "12 March, 2023",
-    endDate: "20 March, 2023",
-    image: "fazenda.jpg",
+    googleMapLink: "https://goo.gl/maps/tiWurNr5nV1u2Xba8",
+    startDate: "20 July, 2023",
+    endDate: "22 July, 2023",
+    image: "gorilla.jpg",
     description:
-      "Fazenda Sengha is an outdoor recreationnal center that offers horse back riding experiences, ziplines, archery, quad bikes, bungee trampoline, pétanque and others.",
+      "Volcanoes National Park is popular because Rwanda is considered by some to be the best place to see gorillas in Africa. The park is home to the intriguing but endangered mountain gorillas",
   },
 ];
 
