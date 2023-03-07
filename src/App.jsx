0539498ca652dd74357ae4b2567d897a36253a65
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-07 15:43:28 +0200

    Remove derived state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import Box from "./components/Box";
+import allBoxes from "./data/boxes";
+
 export default function App() {
+  const [boxes, setBoxes] = useState(allBoxes);
+  const toggle = (boxId) => {
+    setBoxes((prevState) =>
+      prevState.map((box) => (box.id === boxId ? { ...box, on: !box.on } : box))
+    );
+  };
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hello React!</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div className="grid grid-cols-3 gap-2">
+        {boxes.map((box) => (
+          <Box key={box.id} id={box.id} handleClick={toggle} isOn={box.on} />
+        ))}
+      </div>
     </div>
   );
 }
