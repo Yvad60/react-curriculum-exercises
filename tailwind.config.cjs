0df64dd932d6a4314ff1297a34316bc89e11f50c
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-15 17:24:56 +0200

    Add base html and logic implementation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        sans: ["Karla", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
