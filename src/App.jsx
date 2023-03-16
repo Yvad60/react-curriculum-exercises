412431079edceeb3be606cade9bebedb5ec921ac
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-16 12:47:36 +0200

    Add landing base designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import blueBlob from "./assets/blue-blob.svg";
+import yellowBlob from "./assets/yellow-blob.svg";
+import Game from "./components/Game";
+import Landing from "./components/Landing";
+
 export default function App() {
+  const [isGameOpen, setIsGameOpen] = useState(false);
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hello React!</h1>
-    </div>
+    <main className="bg-primary-light">
+      <img
+        src={blueBlob}
+        alt="blue blob"
+        className="absolute bottom-0 left-0 w-[16%]"
+      />
+
+      <img
+        src={yellowBlob}
+        alt="yellow blob"
+        className="absolute top-0 right-0 w-[16%]"
+      />
+      {isGameOpen ? <Game /> : <Landing />}
+    </main>
   );
 }
