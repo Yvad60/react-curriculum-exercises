66213f7bf5e0d976dc8df3c536198fbc78f9a8c3
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-20 16:05:06 +0200

    Add loading state

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 import Button from "./Button";
 import Question from "./Question";
 
@@ -12,18 +13,26 @@ const generateShuffledAnswers = (incorrectAnswers, correctAnswer) => {
   return shuffledAnswers;
 };
 
-const fetchQuestions = async () => {
-  const response = await fetch(API_URL);
-  const responseData = await response.json();
-  return responseData.results;
-};
-
 export default function Game() {
   const [quizQuestions, setQuizQuestions] = useState([]);
   const [isGameWon, setIsGameWon] = useState(false);
   const [canRevealAnswers, setCanRevealAnswers] = useState(false);
   const [questionsData, setQuestionsData] = useState([]);
   const [areAnswersRevealed, setAreAnswersRevealed] = useState(false);
+  const [score, setScore] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const fetchQuestions = async () => {
+    setIsFetching(true);
+    try {
+      const response = await fetch(API_URL);
+      const responseData = await response.json();
+      return responseData.results;
+    } catch (error) {
+    } finally {
+      setIsFetching(false);
+    }
+  };
 
   const selectAnswer = (currentQuestion, selectedAnswer) => {
     setQuestionsData((prevState) =>
@@ -33,14 +42,6 @@ export default function Game() {
           : answer
       )
     );
-  };
-
-  const calculateCorrectAnswers = () => {
-    return questionsData.reduce((totalScore, currentQuestion) => {
-      if (currentQuestion.selectedAnswer === currentQuestion.correctAnswer)
-        return totalScore + 1;
-      return totalScore;
-    }, 0);
   };
 
   const restartCurrentGame = () => {
@@ -88,49 +89,74 @@ export default function Game() {
     if (isAllQuestionsAnswered) setCanRevealAnswers(true);
   }, [questionsData]);
 
+  useEffect(() => {
+    if (areAnswersRevealed) {
+      const score = questionsData.reduce((totalScore, currentQuestion) => {
+        if (currentQuestion.selectedAnswer === currentQuestion.correctAnswer)
+          return totalScore + 1;
+        return totalScore;
+      }, 0);
+      setScore(score);
+      if (score === questionsData.length) setIsGameWon(true);
+    }
+  }, [areAnswersRevealed]);
+
   return (
     <section className="flex flex-col w-3/5 mx-auto mt-6 max-w-[1200px]">
-      {questionsData.map((questionInfo, index) => {
-        const { question, allChoices, selectedAnswer, correctAnswer } =
-          questionInfo;
-        return (
-          <div
-            className="px-2 py-5 border-b hover:bg-slate-100 border-slate-300 last:border-none"
-            key={index}
-          >
-            <Question
-              title={question}
-              answers={allChoices}
-              selectAnswer={selectAnswer}
-              selectedAnswer={selectedAnswer}
-              isAnswersRevealed={areAnswersRevealed}
-              correctAnswer={correctAnswer}
-            />
-          </div>
-        );
-      })}
-
-      {!areAnswersRevealed ? (
-        <Button
-          className="mx-auto disabled:bg-[#a6a7b5] my-6"
-          disabled={!canRevealAnswers}
-          onClick={revealAnswers}
-        >
-          Check answers
-        </Button>
+      {isFetching ? (
+        <h3 className="text-2xl font-semibold">Getting questions...</h3>
       ) : (
-        <div className="flex justify-between items-center my-6">
-          <Button className="w-fit" onClick={restartCurrentGame}>
-            Restart current game
-          </Button>
-          <h2 className="text-xl font-semibold">
-            You scored {calculateCorrectAnswers()}/{questionsData.length}{" "}
-            correct answers
-          </h2>
-          <Button className="w-fit" onClick={startNewGame}>
-            Play a new game
-          </Button>
-        </div>
+        <>
+          {isGameWon && (
+            <Confetti recycle={false} width={1200} className="mx-auto" />
+          )}
+
+          {questionsData.map((questionInfo, index) => {
+            const { question, allChoices, selectedAnswer, correctAnswer } =
+              questionInfo;
+            return (
+              <div
+                className="px-2 py-5 border-b hover:bg-slate-100 border-slate-300 last:border-none"
+                key={index}
+              >
+                <Question
+                  title={question}
+                  answers={allChoices}
+                  selectAnswer={selectAnswer}
+                  selectedAnswer={selectedAnswer}
+                  isAnswersRevealed={areAnswersRevealed}
+                  correctAnswer={correctAnswer}
+                />
+              </div>
+            );
+          })}
+
+          {!areAnswersRevealed ? (
+            <Button
+              className="mx-auto disabled:bg-[#a6a7b5] my-6"
+              disabled={!canRevealAnswers}
+              onClick={revealAnswers}
+            >
+              Check answers
+            </Button>
+          ) : (
+            <div className="flex justify-between items-center my-6">
+              <Button className="w-fit" onClick={restartCurrentGame}>
+                Restart current game
+              </Button>
+              <h2 className="text-xl font-semibold">
+                You scored {score}/{questionsData.length} correct answers
+              </h2>
+              <Button
+                className="w-fit"
+                onClick={startNewGame}
+                disabled={isFetching}
+              >
+                Play a new game
+              </Button>
+            </div>
+          )}
+        </>
       )}
     </section>
   );
