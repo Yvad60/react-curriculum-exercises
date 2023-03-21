0a57353f335f0f8e0d7150b318c1ce841dc39aac
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-21 18:33:05 +0200

    Add state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-export default function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => setCount((prevCount) => prevCount + 1);
+
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hello React!</h1>
-    </div>
+    <main className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-xl">
+          Button has been clicked:{" "}
+          <span className="font-semibold">{count}</span> times.
+        </p>
+        <button
+          className="px-8 py-2 text-lg font-medium text-white bg-blue-800 rounded hover:bg-blue-900"
+          onClick={incrementCount}
+        >
+          Click me
+        </button>
+      </div>
+    </main>
   );
-}
+};
+
+export default App;
