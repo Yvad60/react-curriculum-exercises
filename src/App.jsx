1db06d2c298297a62c1c6b0fec51d8899e3b1684
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-21 18:57:13 +0200

    Add mapping through a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+const App = () => {
+  const animals = [
+    "🐶 dog",
+    "🐈 cat",
+    "🐔 chicken",
+    "🐮 cow",
+    "🐏 sheep",
+    "🐴 horse",
+  ];
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hello React!</h1>
-    </div>
+    <main className="flex items-center justify-center h-screen">
+      <ul className="flex flex-col justify-center text-xl border rounded-md border-gray-300">
+        {animals.map((animal, index) => (
+          <li
+            className="w-full px-16 py-4 text-center border-b border-gray-300"
+            key={index}
+          >
+            {animal}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
-}
+};
+
+export default App;
