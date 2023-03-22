519d2d780da65d02d062ec07b120441a69b0fe95
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-22 15:54:17 +0200

    Add ids to the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { AnimalListItem } from "./components/AnimalListItem";
+import AnimalList from "./components/AnimalList";
 
 const App = () => {
-  const animals = [
-    "🐶 dog",
-    "🐈 cat",
-    "🐔 chicken",
-    "🐮 cow",
-    "🐏 sheep",
-    "🐴 horse",
-  ];
-
   return (
     <main className="flex items-center justify-center h-screen">
-      <ul className="flex flex-col justify-center text-xl border rounded-md border-gray-300">
-        {animals.map((animal, index) => (
-          <AnimalListItem key={index}>{animal}</AnimalListItem>
-        ))}
-      </ul>
+      <AnimalList />
     </main>
   );
 };
--- src/components/AnimalListItem.jsx
+++ src/components/AnimalListItem.jsx
@@ -1,7 +1,9 @@
-export const AnimalListItem = ({ children }) => {
+const AnimalListItem = ({ children }) => {
   return (
     <li className="w-full px-16 py-4 text-center border-b border-gray-300">
       {children}
     </li>
   );
 };
+
+export default AnimalListItem;
