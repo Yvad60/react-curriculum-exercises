18d13e44c2be3f9b5109b38dc90d159bff8b4cab
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-22 12:10:08 +0200

    Remove unnecessary space

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
     <main className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center gap-4">
         <p className="text-xl">
-          Button has been clicked:{" "}
-          <span className="font-semibold">{count}</span> times.
+          Button has been clicked:
+          <span className="font-semibold ml-1">{count}</span> times.
         </p>
         <button
           className="px-8 py-2 text-lg font-medium text-white bg-blue-800 rounded hover:bg-blue-900"
