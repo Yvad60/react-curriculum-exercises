b67dfad3093e365ea756acd5cde668319dbb22f3
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-30 13:01:46 +0200

    complete react todo (not refactored)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import { TodoRow } from "./components/TodoRow";
+import { createTodo } from "./helpers";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [isTodoInvalid, setIsTodoInvalid] = useState(false);
+
+  const handleTaskInputChange = (event) => {
+    setIsTodoInvalid(false);
+    setNewTask(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (!newTask) return setIsTodoInvalid(true);
+    setTodos((prevState) => [createTodo(newTask), ...prevState]);
+    setNewTask("");
+  };
+
+  const handleSaveByEnter = (event) => {
+    if (event.key === "Enter") addTodo();
+  };
+
+  const handleTodoDelete = (todoId) =>
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== todoId));
+
+  const editTodo = (todoId, field, newValue) =>
+    setTodos((prevState) =>
+      prevState.map((todo) =>
+        todo.id === todoId ? { ...todo, [field]: newValue } : todo
+      )
+    );
+
   return (
     <main className="flex justify-center min-h-screen py-10 bg-slate-100">
       <div className="w-full max-w-[800px]">
@@ -8,14 +39,43 @@ const App = () => {
           Todo
         </h1>
         <div className="flex w-full px-5 py-3 text-lg bg-white border border-gray-300 shadow-md outline-none rounded-3xl">
-          <input type="text" className="w-full outline-none " />
-          <button className="bg-[#018d81] h-8 w-[34px] text-white text-3xl flex justify-center items-center rounded-full">
+          <input
+            type="text"
+            className="w-full outline-none"
+            placeholder="Type task here..."
+            value={newTask}
+            onChange={handleTaskInputChange}
+            onKeyDown={handleSaveByEnter}
+          />
+
+          <button
+            className="bg-[#018d81] h-8 w-[34px] text-white text-3xl flex justify-center items-center rounded-full"
+            onClick={addTodo}
+          >
             +
           </button>
         </div>
-        <div className="mt-6">
-          <TodoRow />
-          <TodoRow />
+        {isTodoInvalid && (
+          <p className="text-red-500 text-sm mt-2 text-center">
+            Todo can not be empty
+          </p>
+        )}
+
+        <div className="mt-5">
+          {todos.length > 0 ? (
+            todos.map((todo) => (
+              <TodoRow
+                deleteTodo={() => handleTodoDelete(todo.id)}
+                todo={todo}
+                editTodo={editTodo}
+                key={todo.id}
+              />
+            ))
+          ) : (
+            <h2 className="text-xl mt-10 font-semibold text-center text-gray-700">
+              What is your main focus today?
+            </h2>
+          )}
         </div>
       </div>
     </main>
