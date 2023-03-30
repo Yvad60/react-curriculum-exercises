e1785f27dae21671b71b15ca030729492431413a
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-30 09:40:53 +0200

    Update src/App.jsx
    Co-authored-by: MugaboDenys <74446950+MugaboDenys@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import jokes from "./data/jokes";
 const App = () => {
   return (
     <section className="flex justify-center items-center h-screen bg-[#e0f7dd] gap-10">
-      {jokes.map((joke) => (
-        <JokeCard {...joke} key={joke.id} />
-      ))}
+      {jokes.map(joke => (<JokeCard {...joke} key={joke.id} />))}
     </section>
   );
 };
