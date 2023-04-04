ba984ba9420e9a4ec52b5486d599b52db7b59f49
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-04-04 20:49:12 +0200

    Implement exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import { connect } from "react-redux";
+import NavBar from "./components/NavBar";
+import ProductCard from "./components/ProductCard";
+
+function App({ products }) {
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-500">Hello React!</h1>
-    </div>
+    <main className="flex justify-center bg-[#fff7ef] min-h-screen">
+      <div>
+        <NavBar />
+        <div className="flex flex-wrap justify-center w-5/6 gap-10 mx-auto mt-10 ">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
+
+const mapState = (state) => ({ products: state.products });
+
+export default connect(mapState)(App);
