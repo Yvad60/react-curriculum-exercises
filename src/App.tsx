c0592f23dcb6f837c0f7d0668e7d5c28df18d6e3
ivad <yvesyvad@gmail.com>
2023-05-30 11:08:44 +0200

    Add typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-export default function App() {
+const App: React.FC = () => {
   return (
     <main className="h-screen flex justify-center items-center bg-[#fec800]">
       <p className="text-2xl text-gray-700">Hello world!</p>
     </main>
   );
-}
+};
+
+export default App;
